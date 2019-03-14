2e242d40b3771425825b9edfec67d9688e80c45b
thomas.farruggia9@etu.univ-lorraine.fr <thomas.farruggia9@etu.univ-lorraine.fr>
2019-03-14 11:54:15 +0100

    correction du Noeud

---
+++ Noeud.java
@@ -0,0 +1,23 @@
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+public class Noeud {
+public static void main (String [] args) throws RemoteException, NotBoundException
+{
+	
+	Distributeur dis = new Distributeur();
+	
+	Registry reg = LocateRegistry.getRegistry(args[0], Integer.parseInt(args[1]) );
+
+	
+	ServiceTexte rd = (ServiceTexte) reg.lookup("Phrases");
+	
+	ServiceDistributeur st = (ServiceDistributeur) UnicastRemoteObject.exportObject(dis,0);
+	
+	
+	rd.enregistrerNoeud(st);
+
+}
+}
